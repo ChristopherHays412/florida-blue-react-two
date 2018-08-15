0523e7c6deda1131685e398a31247b7d407c79be
Christopher Hays <31630769+ChristopherHays412@users.noreply.github.com>
2018-08-15 12:13:02 -0400

    Initial commit.

---
+++ src/components/User.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class User extends Component {
+  deleteUser(id, e) {
+    e.preventDefault();
+    this.props.onDelete(id);
+  }
+
+  render() {
+    return (
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.username}</td>
+        <td>{this.props.email}</td>
+        <td>
+          <a
+            href="#"
+            className="text-danger"
+            onClick={this.deleteUser.bind(this, this.props.id)}
+          >
+            X
+          </a>
+        </td>
+      </tr>
+    );
+  }
+}
